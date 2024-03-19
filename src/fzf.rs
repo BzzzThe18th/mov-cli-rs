7c4cd66077d4da1e434fbb8dfcb92dd077355f29
jeighsihgohjdrohjocdh <69125495+BzzzThe18th@users.noreply.github.com>
2024-03-19 01:30:33 -0500

    lots n lots of stuff

---
+++ src/fzf.rs
@@ -0,0 +1,219 @@
+use fzf_wrapped::*;
+use crate::player::play_episode;
+use crate::structs::{Args, SeriesInfo, SeriesResult};
+use crate::scraping::{search, get_series_info, get_season_info};
+use reqwest::blocking::Client;
+
+static SERIES_OPTIONS: [&str; 2] = ["search again", "quit"];
+static OTHER_OPTIONS: [&str; 3] = ["back", "search again", "quit"];
+
+fn prompt_search(args: &Args, client: &Client) {
+    print!("Search for:");
+    let input: &String = &text_io::read!("{}\n");
+    let search = search(&client, input).unwrap();
+    if !args.first {
+        display_series(args, &client, &search.results)
+    } else {
+        let result = search.results[0].to_owned();
+        if result.media_type == "tv" {
+            display_seasons(args, &client, &result, &search.results);
+        }
+    }
+}
+
+pub fn display_series(args: &Args, client: &Client, search_results: &Vec<SeriesResult>) {
+    let fzf = Fzf::builder()
+        .layout(Layout::Reverse)
+        .border(Border::Rounded)
+        .border_label("mov-cli-rs")
+        .color(Color::Dark)
+        .header("Select a series/movie")
+        .header_first(true)
+        .build()
+        .unwrap();
+
+    let mut series_names = vec![String::new(); (search_results.len() - 1) as usize];
+    for i in 0..(search_results.len() - 1) as usize {
+        let result = search_results[i].to_owned();
+        if search_results[i].title.is_none() {series_names[i] = result.name.unwrap()} else {series_names[i] = result.title.unwrap()}
+        
+        let mut append_str = String::new();
+        if result.first_air_date.is_none() && result.release_date.is_none() {
+            append_str = " (N/A)".to_string();
+        } else if !result.first_air_date.is_none() {
+            let mut date = result.first_air_date.unwrap();
+            if !date.is_empty() {
+                let air_year = format!(" ({})", date.split_at_mut(4).0);
+                append_str = air_year;
+            }
+        } else if !result.release_date.is_none() {
+            let mut date = result.release_date.unwrap();
+            if !date.is_empty() {
+                let release_year = format!(" ({})", date.split_at_mut(4).0);
+                append_str = release_year;
+            }
+        }
+        
+        series_names[i].push_str(append_str.as_str());
+    }
+
+    let mut _series_options = vec![String::new(); SERIES_OPTIONS.len()];
+    for j in 0.._series_options.len() {
+        _series_options[j] = SERIES_OPTIONS[j].to_string();
+    }
+
+    series_names.append(&mut _series_options);
+    let series_name: String = run_with_output(fzf, series_names).unwrap();
+
+    if SERIES_OPTIONS.contains(&series_name.as_str()) {
+        match series_name.as_str() {
+            "search again" => {
+                prompt_search(args, client);
+            }
+            "quit" => {
+                return;
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    let mut series_index = 0;
+    for k in 0..search_results.len() {
+        let result = search_results[k].to_owned();
+        let mut append_str = String::new();
+        if result.first_air_date.is_none() && result.release_date.is_none() {
+            append_str = " (N/A)".to_string();
+        } else if !result.first_air_date.is_none() {
+            let mut date = result.first_air_date.unwrap();
+            if !date.is_empty() {
+                let air_year = format!(" ({})", date.split_at_mut(4).0);
+                append_str = air_year;
+            }
+        } else if !result.release_date.is_none() {
+            let mut date = result.release_date.unwrap();
+            if !date.is_empty() {
+                let release_year = format!(" ({})", date.split_at_mut(4).0);
+                append_str = release_year;
+            }
+        }
+
+        let mut name = if result.title.is_none() {result.name.unwrap()} else {result.title.unwrap()};
+        name.push_str(&append_str);
+        if name == series_name {series_index=k}
+    }
+    let series = search_results[series_index].to_owned();
+    
+    if args.season == -1 {
+        if series.media_type == "tv" {
+            display_seasons(args, client, &series, &search_results);
+        } else {
+            play_episode(client, &series, 1, 1);
+        }
+    } else {
+        //episode is not specified
+        if args.episode == -1 {
+            display_episodes(args, client, &series, args.season, &search_results);
+        } else {
+            play_episode(client, &series, args.season, args.episode);
+        }
+    }
+}
+
+pub fn display_seasons(args: &Args, client: &Client, series: &SeriesResult, search_results: &Vec<SeriesResult>) {
+    let fzf = Fzf::builder()
+        .layout(Layout::Reverse)
+        .border(Border::Rounded)
+        .border_label("mov-cli-rs")
+        .color(Color::Bw)
+        .header("Select a season")
+        .header_first(true)
+        .build()
+        .unwrap();
+
+    let series_info: SeriesInfo = get_series_info(&client, series.id).unwrap();
+    let seasons = series_info.seasons;
+
+    let mut season_names = vec![String::new(); seasons.clone().len()];
+    for i in 0..seasons.clone().len() {
+        let season = seasons[i].to_owned();
+        season_names[i] = season.name;
+        
+        let append_str = format!(" ({})", season.air_date.split_at(4).0);
+        
+        season_names[i].push_str(append_str.as_str());
+    }
+    
+    let mut _season_options = vec![String::new(); OTHER_OPTIONS.len()];
+    for j in 0.._season_options.len() {
+        _season_options[j] = OTHER_OPTIONS[j].to_string();
+    }
+    season_names.append(&mut _season_options);
+    let season_name: String = run_with_output(fzf, season_names).unwrap();
+
+    if OTHER_OPTIONS.contains(&season_name.as_str()) {
+        match season_name.as_str() {
+            "back" => {
+                display_series(args, client, &search_results);
+            }
+            "search again" => {
+                prompt_search(args, client);
+            }
+            "quit" => {
+                return;
+            }
+            _ => {
+                panic!();
+            }
+        }
+    }
+
+    let series_index = seasons.iter().position(|r| [r.name.to_owned(), " (".to_string(), r.air_date.split_at(4).0.to_owned(), ")".to_string()].concat() == season_name).unwrap();
+    let season = seasons[series_index].clone();
+
+    //episode is not specified
+    if args.episode == -1 {
+        display_episodes(args, client, series, season.season_number, &search_results);
+    } else {
+        play_episode(client, series, season.season_number, args.episode);
+    }
+}
+
+pub fn display_episodes(args: &Args, client: &Client, series: &SeriesResult, season: i32, search_results: &Vec<SeriesResult>) {
+    let fzf = Fzf::builder()
+        .layout(Layout::Reverse)
+        .border(Border::Rounded)
+        .border_label("mov-cli-rs")
+        .color(Color::Bw)
+        .header("Pick an episode")
+        .header_first(true)
+        .build()
+        .unwrap();
+
+    let episodes = get_season_info(&client, series.id, season).unwrap().episodes;
+
+    let mut episode_nums = vec![String::new(); episodes.len()];
+    for i in 0..episodes.len() {
+        episode_nums[i] = episodes[i].episode_number.to_string();
+    }
+
+    let mut _episode_options = vec![String::new(); OTHER_OPTIONS.len()];
+    for j in 0.._episode_options.len() {
+        _episode_options[j] = OTHER_OPTIONS[j].to_string();
+    }
+    episode_nums.append(&mut _episode_options);
+
+    let episode_num = run_with_output(fzf, episode_nums).unwrap();
+
+    if OTHER_OPTIONS.contains(&episode_num.as_str()) {
+        match episode_num.as_str() {
+            "back" => {display_seasons(args, client, &series, search_results)}
+            "search again" => {prompt_search(args, client)}
+            "quit" => {return;}
+            _ => {panic!();}
+        }
+    }
+
+    play_episode(client, series, season, episode_num.parse::<i32>().unwrap());
+}
--- src/scraping.rs
+++ src/scraping.rs
@@ -1,75 +1,9 @@
 use reqwest::blocking;
 use std::error::Error;
+use crate::structs::{APISourceResults, SearchResults, SeasonData, SeriesInfo, SeriesResult};
 use crate::constants::{self, BRAFLIX_API, TMDB_API, TMDB_BRAFLIX_API_KEY};
 
-#[derive(serde::Deserialize, Clone, Debug)]
-pub struct SeriesResult {
-    pub adult: bool,
-    pub id: i32,
-    pub name: Option<String>,
-    pub title: Option<String>,
-    pub original_language: Option<String>,
-    pub media_type: String,
-    pub release_date: Option<String>,
-    pub first_air_date: Option<String>,
-}
-
-#[derive(serde::Deserialize)]
-pub struct SearchResults {
-    pub page: i32,
-    pub results: Vec<SeriesResult>,
-    pub total_pages: i32,
-    pub total_results: i32,
-}
-
-#[derive(serde::Deserialize, Clone, Debug)]
-struct SubtitleTrack {
-    // url: Option<String>,
-    // file: Option<String>,
-    lang: Option<String>,
-    language: Option<String>,
-}
-
-#[derive(serde::Deserialize, Clone, Debug)]
-struct APISource {
-    url: String,
-    quality: String,
-}
-
-#[derive(serde::Deserialize, Clone, Debug)]
-struct APISourceResults {
-    sources: Option<Vec<APISource>>,
-    subtitles: Option<Vec<SubtitleTrack>>,
-}
-
-#[derive(serde::Deserialize)]
-pub struct EpisodeData {
-    pub episode_number: i32,
-    pub episode_name: Option<String>,
-    pub overview: Option<String>,
-}
-
-#[derive(serde::Deserialize)]
-pub struct SeasonData {
-    pub episodes: Vec<EpisodeData>,
-}
-
-#[derive(serde::Deserialize, Clone)]
-pub struct SeasonInfo {
-    pub episode_count: i32,
-    pub id: i32,
-    pub name: String,
-    pub season_number: i32,
-    pub air_date: String,
-}
-
-#[derive(serde::Deserialize)]
-pub struct SeriesInfo {
-    pub number_of_seasons: i32,
-    pub seasons: Vec<SeasonInfo>,
-}
-
-fn try_get_link_from_api_source(provider: String, client: blocking::Client, series: SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
+fn try_get_link_from_api_source(provider: String, client: &blocking::Client, series: SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
     let url = format!("{0}/{1}/sources-with-title?title={2}&mediaType={3}&episodeId={4}&seasonId={5}&tmdbId={6}", BRAFLIX_API, provider, if series.name.is_none() {series.title.unwrap()} else {series.name.unwrap()}, series.media_type, episode, season, series.id);
     let json: APISourceResults = client.get(&url)
         .send()?
@@ -96,26 +30,26 @@ fn try_get_link_from_api_source(provider: String, client: blocking::Client, seri
     Ok("".to_string())
 }
 
-fn get_link_from_api_source(client: blocking::Client, series: SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
-    let res = try_get_link_from_api_source("flixhq".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|result|
+fn get_link_from_api_source(client: &blocking::Client, series: &SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
+    let res = try_get_link_from_api_source("flixhq".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|result|
         if result.ends_with(".m3u8") { Ok(result) } else { 
             println!("Failed to fetch with Braflix API using provider FlixHQ: {}", if result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-            try_get_link_from_api_source("vidsrc".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|vidsrc_result|{
+            try_get_link_from_api_source("vidsrc".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|vidsrc_result|{
                 if vidsrc_result.ends_with(".m3u8") {Ok(vidsrc_result)} else {
                     println!("Failed to fetch with Braflix API using provider VidSrc: {}", if vidsrc_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                    try_get_link_from_api_source("vidsrcto".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|vidsrcto_result|{
+                    try_get_link_from_api_source("vidsrcto".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|vidsrcto_result|{
                         if vidsrcto_result.ends_with(".m3u8") {Ok(vidsrcto_result)} else {
                             println!("Failed to fetch with Braflix API using provider VidSrcTo: {}", if vidsrcto_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                            try_get_link_from_api_source("superstream".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|superstream_result|{
+                            try_get_link_from_api_source("superstream".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|superstream_result|{
                                 if superstream_result.ends_with(".m3u8") { Ok(superstream_result) } else {
                                     println!("Failed to fetch with Braflix API using provider SuperStream: {}", if superstream_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                                    try_get_link_from_api_source("febbox".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|febbox_result|{
+                                    try_get_link_from_api_source("febbox".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|febbox_result|{
                                         if febbox_result.ends_with(".m3u8") { Ok(febbox_result) } else {
                                             println!("Failed to fetch with Braflix API using provider Febbox: {}", if febbox_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                                            try_get_link_from_api_source("overflix".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|overflix_result|{
+                                            try_get_link_from_api_source("overflix".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|overflix_result|{
                                                 if overflix_result.ends_with(".m3u8") {Ok(overflix_result)} else {
                                                     println!("Failed to fetch with Braflix API using provider OverFlix: {}", if overflix_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                                                    try_get_link_from_api_source("visioncine".to_string(), client.clone(), series.clone(), season.clone(), episode.clone()).and_then(|visioncine_result|{
+                                                    try_get_link_from_api_source("visioncine".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|visioncine_result|{
                                                         println!("Failed to fetch with Braflix API using provider VisionCine: {}", if visioncine_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
                                                         Ok("null".to_string())
                                                     })
@@ -152,13 +86,13 @@ pub fn get_series_info(client: &blocking::Client, show_id: i32) -> Result<Series
     Ok(json)
 }
 
-pub fn get_link(client: blocking::Client, series: SeriesResult, season: i32, episode: i32) -> Result<String, Box<dyn Error>> {
+pub fn get_link(client: &blocking::Client, series: &SeriesResult, season: i32, episode: i32) -> Result<String, Box<dyn Error>> {
     let link = get_link_from_api_source(client, series, season.to_string(), episode.to_string());
     Ok(link.unwrap())
 }
 
-pub fn search(client: &blocking::Client, search_term: String) -> Result<SearchResults, Box<dyn Error>> {
-    let json: SearchResults = client.get([TMDB_API, "/3/search/multi?language=en&page=1&query=", &search_term, "&api_key=", TMDB_BRAFLIX_API_KEY].concat())
+pub fn search(client: &blocking::Client, search_term: &String) -> Result<SearchResults, Box<dyn Error>> {
+    let json: SearchResults = client.get([TMDB_API, "/3/search/multi?language=en&page=1&query=", search_term, "&api_key=", TMDB_BRAFLIX_API_KEY].concat())
         .send()?
         .json()
         .unwrap();
