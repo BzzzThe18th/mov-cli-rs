27bb15fdafba598de2c76ca94a2535e27ad99a6e
jeighsihgohjdrohjocdh <69125495+BzzzThe18th@users.noreply.github.com>
2024-03-20 03:18:46 -0500

    wrong branch :(

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub const AGENT: &str ="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0";
 pub const BRAFLIX_REFR: &str ="https://www.braflix.video";
-pub const BRAFLIX_API: &str ="https://api.braflix.video";
+pub const BRAFLIX_API: &str ="https://providers.braflix.video";
 pub const TMDB_API: &str = "https://api.themoviedb.org";
 pub const TMDB_BRAFLIX_API_KEY: &str = "d39245e111947eb92b947e3a8aacc89f";
