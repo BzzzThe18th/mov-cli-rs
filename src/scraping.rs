46c018a5612d85c92e7b174aeecaa8400f68f9d7
jeighsihgohjdrohjocdh <69125495+BzzzThe18th@users.noreply.github.com>
2024-03-21 19:34:24 -0500

    improve playlist scrape logging

--- src/scraping.rs
+++ src/scraping.rs
@@ -1,9 +1,11 @@
 use reqwest::blocking;
 use std::error::Error;
+use std::time::Duration;
 use crate::structs::{APISourceResults, SearchResults, SeasonData, SeriesInfo, SeriesResult};
 use crate::constants::{self, BRAFLIX_API, TMDB_API, TMDB_BRAFLIX_API_KEY};
 
 fn try_get_link_from_api_source(provider: String, client: &blocking::Client, series: SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
+    print!("trying to fetch from provider {}... ", provider);
     let url = format!("{0}/{1}/sources-with-title?title={2}&mediaType={3}&episodeId={4}&seasonId={5}&tmdbId={6}", BRAFLIX_API, provider, if series.name.is_none() {series.title.unwrap()} else {series.name.unwrap()}, series.media_type, episode, season, series.id);
     let json: APISourceResults = client.get(&url)
         .send()?
@@ -15,43 +17,40 @@ fn try_get_link_from_api_source(provider: String, client: &blocking::Client, ser
         sources.sort_by_key(|k| k.quality.to_lowercase().replace("auto", "0").replace("p", "").parse::<i32>().unwrap());
         sources.reverse();
 
-        //implement subtitle stuff
-        let subs = json.subtitles.unwrap();
-        for sub_track in 0..subs.len() {
-            println!("Subtitle found with language string {}", subs[sub_track].clone().lang.unwrap_or_else(|| {subs[sub_track].clone().language.expect("Failed to get both languages for sub track")}));
-        }
+        //TODO: implement subtitle stuff
+        // let subs = json.subtitles.unwrap();
+        // for sub_track in 0..subs.len() {
+        //     println!("Subtitle found with language string {}", subs[sub_track].clone().lang.unwrap_or_else(|| {subs[sub_track].clone().language.expect("Failed to get both languages for sub track")}));
+        // }
 
-        if sources[0].url.ends_with(".m3u8") { return Ok(sources[0].url.to_string()) } else {
-            //aaaaaaaaaaaaaaaaaaaa ok so seems like braflix encrypts the already thrice encrypted vidsrcto URLs so I'm just gonna rip directly...
-            //TODO: rip directly from encrypted source
-            println!("Resolving encrypted source URLs is not yet implemented, sorry!");
+        if sources[0].url.ends_with(".m3u8") {
+            print!("success - playing with highest quality ({})\n", sources[0].quality.to_lowercase());
+            std::thread::sleep(Duration::from_millis(2500));
+            return Ok(sources[0].url.to_string())
+        } else {
+            print!("failed - encrypted result\n");
+            return Ok(String::from(""));
         }
     }
-    Ok("".to_string())
+    print!("failed - unknown error\n");
+    Ok(String::from(""))
 }
 
 fn get_link_from_api_source(client: &blocking::Client, series: &SeriesResult, season: String, episode: String) -> Result<String, Box<dyn Error>> {
     let res = try_get_link_from_api_source("flixhq".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|result|
         if result.ends_with(".m3u8") { Ok(result) } else { 
-            println!("Failed to fetch with Braflix API using provider FlixHQ: {}", if result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
             try_get_link_from_api_source("vidsrc".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|vidsrc_result|{
                 if vidsrc_result.ends_with(".m3u8") {Ok(vidsrc_result)} else {
-                    println!("Failed to fetch with Braflix API using provider VidSrc: {}", if vidsrc_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
                     try_get_link_from_api_source("vidsrcto".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|vidsrcto_result|{
                         if vidsrcto_result.ends_with(".m3u8") {Ok(vidsrcto_result)} else {
-                            println!("Failed to fetch with Braflix API using provider VidSrcTo: {}", if vidsrcto_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
                             try_get_link_from_api_source("superstream".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|superstream_result|{
                                 if superstream_result.ends_with(".m3u8") { Ok(superstream_result) } else {
-                                    println!("Failed to fetch with Braflix API using provider SuperStream: {}", if superstream_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
                                     try_get_link_from_api_source("febbox".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|febbox_result|{
                                         if febbox_result.ends_with(".m3u8") { Ok(febbox_result) } else {
-                                            println!("Failed to fetch with Braflix API using provider Febbox: {}", if febbox_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
                                             try_get_link_from_api_source("overflix".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|overflix_result|{
                                                 if overflix_result.ends_with(".m3u8") {Ok(overflix_result)} else {
-                                                    println!("Failed to fetch with Braflix API using provider OverFlix: {}", if overflix_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                                                    try_get_link_from_api_source("visioncine".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|visioncine_result|{
-                                                        println!("Failed to fetch with Braflix API using provider VisionCine: {}", if visioncine_result.is_empty() {"Errored while attempting to get link from provider"} else {"Resulting highest quality URL was still encrypted"});
-                                                        Ok("null".to_string())
+                                                    try_get_link_from_api_source("visioncine".to_string(), client, series.clone(), season.clone(), episode.clone()).and_then(|_visioncine_result|{
+                                                        Ok(String::from(""))
                                                     })
                                                 }
                                             })
